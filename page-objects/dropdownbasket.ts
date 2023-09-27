b696ffce8a1040bf6100a77efb8389b2b4e32354
EugeneKarpovichq <jennykarpovich@yandex.by>
2023-09-27 15:09:29 +0300

    Update pos

---
+++ page-objects/dropdownbasket.ts
@@ -0,0 +1,341 @@
+import { Page } from "@playwright/test";
+import MainPagePO from "./main-page";
+import BasketPO from "./basket";
+
+export default class DropDownBasketPO {
+  private page: Page;
+  private mainPage: MainPagePO;
+  private basket: BasketPO;
+
+  private dropdownBasket = "[aria-labelledby='dropdownBasket']";
+  private clearBasketBtn = "//*[@id='basketContainer']/div[2]/div[3]/a";
+  private goToBasketSelector = "//*[@id='basketContainer']/div[2]/div[2]/a";
+  private basketItemsCount =
+    "//span[@class='basket-count-items badge badge-primary']";
+  private noPromoProductSelector =
+    "//div[@class='note-list row']//div[@class='note-item card h-100']";
+  private promoProductSelector =
+    "//div[@class='note-list row']//div[contains(@class, 'hasDiscount')]";
+  private buyBtnSelector = "*[class^='actionBuyProduct']:has-text('Купить')";
+  private productPriceSelector = "*[class^='product_price']";
+  private productBasketName = "*[class^='basket-item-title']";
+  private productBasketPrice = "*[class^='basket-item-price']";
+  private productBasketCount = "*[class^='basket-item-count']";
+  private productBasketBalance = "*[class^='basket_price']";
+  private amountField = "*[name^='product-enter-count']";
+  private productData: { [key: string]: { price: string; count: string } } = {};
+
+  constructor(page: Page) {
+    this.page = page;
+    this.mainPage = new MainPagePO(page);
+  }
+
+  async isProductInBasket(
+    productName: string,
+    productPrice: string,
+    productCount: number = 1
+  ) {
+    await this.getProductDataInBasket();
+
+    const productInfo = this.productData[productName];
+
+    if (!productInfo) {
+      return false; // Продукт не найден в корзине
+    }
+
+    // Преобразование productPrice в числовое значение (если оно не было числом)
+    const numericInputPrice = parseFloat(productPrice);
+
+    // Преобразование productInfo.count в число
+    const countInBasket = parseInt(productInfo.count, 10);
+
+    // Проверка цены и количества продуктов
+    const isPriceEqual = numericInputPrice === numericInputPrice;
+    const isCountEqual = countInBasket === productCount;
+
+    return isPriceEqual && isCountEqual;
+  }
+
+  async getProductDataInBasket() {
+    const names = await this.getProductBasketName();
+    const prices = await this.getProductBasketPrice();
+    const counts = await this.getProductBasketCount();
+
+    // Построение объекта данных о продуктах
+    for (let i = 0; i < names.length; i++) {
+      this.productData[names[i]] = {
+        price: prices[i],
+        count: counts[i],
+      };
+    }
+  }
+
+  async getTotalBasketPrice() {
+    const productNames = await this.getProductBasketName();
+    const productPrices = await this.getProductBasketPrice();
+
+    let totalPrice = 0;
+
+    for (let i = 0; i < productNames.length; i++) {
+      const price = parseFloat(productPrices[i].match(/\d+/)[0]);
+      totalPrice += price;
+    }
+
+    return totalPrice;
+  }
+
+  async getActualTotalPrice() {
+    const totalPriceText = await this.page.textContent(
+      this.productBasketBalance
+    );
+    const numberTotalPrice = parseFloat(totalPriceText.replace(/[^\d.]/g, ""));
+
+    return numberTotalPrice;
+  }
+
+  async getProductBasketName() {
+    const productNames = await this.page.$$eval(
+      this.productBasketName,
+      (elements) => {
+        return elements
+          .filter((element) => element.textContent !== null)
+          .map((element) => element.textContent as string);
+      }
+    );
+    return productNames;
+  }
+
+  async getProductBasketPrice() {
+    const productPrices = await this.page.$$eval(
+      this.productBasketPrice,
+      (elements) => {
+        return elements
+          .filter((element) => element.textContent !== null)
+          .map((element) => {
+            const text = element.textContent as string;
+            const match = text.match(/(\d+)/);
+            if (match) {
+              return match[0];
+            } else {
+              return "0"; // Возвращаем '0', если число не найдено
+            }
+          });
+      }
+    );
+    return productPrices;
+  }
+
+  async getProductBasketCount() {
+    const productCounts = await this.page.$$eval(
+      this.productBasketCount,
+      (elements) => {
+        return elements
+          .filter((element) => element.textContent !== null)
+          .map((element) => element.textContent as string);
+      }
+    );
+    return productCounts;
+  }
+
+  async addNoPromoProductByName(productName: string, quantity: number = 1) {
+    const productElements = await this.page.$$(this.noPromoProductSelector);
+
+    for (const productElement of productElements) {
+      const textContent = await productElement?.textContent();
+
+      if (textContent && textContent.includes(productName)) {
+        const buyButton = await productElement.$(this.buyBtnSelector);
+
+        if (buyButton) {
+          const amountField = await productElement.$(this.amountField);
+          if (amountField) {
+            await amountField.fill(quantity.toString());
+          }
+
+          await buyButton.click();
+          await this.page.waitForResponse((response) => {
+            return (
+              response.url() === "https://enotes.pointschool.ru/basket/get" &&
+              response.status() === 200
+            );
+          });
+          return;
+        }
+      }
+    }
+
+    throw new Error(`Не удалось найти элемент с именем: ${productName}`);
+  }
+
+  async addPromoProductByName(productName: string, quantity: number = 1) {
+    const productElements = await this.page.$$(this.promoProductSelector);
+
+    for (const productElement of productElements) {
+      const textContent = await productElement?.textContent();
+
+      if (textContent && textContent.includes(productName)) {
+        const buyButton = await productElement.$(this.buyBtnSelector);
+
+        if (buyButton) {
+          const amountField = await productElement.$(this.amountField);
+          if (amountField) {
+            // Задаем количество продуктов в поле amountField
+            await amountField.fill(quantity.toString());
+          }
+
+          await buyButton.click();
+          await this.page.waitForResponse((response) => {
+            return (
+              response.url() === "https://enotes.pointschool.ru/basket/get" &&
+              response.status() === 200
+            );
+          });
+          return;
+        }
+      }
+    }
+
+    throw new Error(`Не удалось найти элемент с именем: ${productName}`);
+  }
+
+  async getPriceForProduct(productName: string) {
+    const productElements = await this.page.$$(this.noPromoProductSelector);
+
+    for (const productElement of productElements) {
+      const textContent = await productElement?.textContent();
+
+      if (textContent && textContent.includes(productName)) {
+        const productPriceElement = await productElement.$(
+          this.productPriceSelector
+        );
+
+        if (productPriceElement) {
+          const productPriceText = await productPriceElement.textContent();
+          const match = productPriceText.match(/\d+/); // Извлекаем числовую часть
+          if (match) {
+            return match[0];
+          }
+        }
+      }
+    }
+
+    throw new Error(
+      `Не удалось найти цену для продукта с именем: ${productName}`
+    );
+  }
+
+  async getPriceForPromoProduct(productName: string) {
+    const productElements = await this.page.$$(this.promoProductSelector);
+
+    for (const productElement of productElements) {
+      const textContent = await productElement?.textContent();
+
+      if (textContent && textContent.includes(productName)) {
+        const productPriceElement = await productElement.$(
+          this.productPriceSelector
+        );
+
+        if (productPriceElement) {
+          const productPriceText = await productPriceElement.textContent();
+          const match = productPriceText.match(/\d+/); // Извлекаем числовую часть
+          if (match) {
+            return match[0];
+          }
+        }
+      }
+    }
+
+    throw new Error(
+      `Не удалось найти цену для продукта с именем: ${productName}`
+    );
+  }
+
+  async goToBasketPage() {
+    await this.page.click(this.goToBasketSelector);
+  }
+
+  async isBasketPageOpened() {
+    const currentUrl = this.page.url();
+    const isBasketPage = currentUrl.endsWith("/basket");
+
+    if (!isBasketPage) {
+      return false;
+    }
+
+    const isErrorMessageDisplayed =
+      await this.basket.isErrorMessageNotDisplayed();
+
+    if (!isErrorMessageDisplayed) {
+      throw new Error("На странице корзины отображается сообщение об ошибке.");
+    }
+
+    return true;
+  }
+
+  async clearBasket() {
+    const isCleared = await this.isBasketCleared();
+    if (isCleared) {
+      console.log("Корзина пуста, операция очистки корзины не требуется.");
+    } else {
+      await this.mainPage.clickBasketBtn();
+
+      try {
+        await this.page.waitForSelector(this.dropdownBasket, {
+          state: "visible",
+          timeout: 5000,
+        });
+      } catch (error) {
+        console.log("Выпадающий список корзины не открылся.");
+      }
+
+      await this.page.click(this.clearBasketBtn);
+      await this.page.waitForResponse((response) => {
+        return (
+          response.url() === "https://enotes.pointschool.ru/basket/get" &&
+          response.status() === 200
+        );
+      });
+    }
+  }
+
+  async isBasketCleared() {
+    const cartItemCountText = await this.page.textContent(
+      this.basketItemsCount
+    );
+
+    if (cartItemCountText === "0") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async countProductsInBasket() {
+    const cartItemCountText = await this.page.textContent(
+      this.basketItemsCount
+    );
+    return cartItemCountText;
+  }
+
+  async isDropDownBasketOpened() {
+    try {
+      await this.page.waitForSelector(this.dropdownBasket, {
+        state: "visible",
+        timeout: 1000,
+      });
+
+      // Проверка, не отображается ли сообщение об ошибке
+      const isErrorMessageHidden =
+        await this.basket.isErrorMessageNotDisplayed();
+
+      if (!isErrorMessageHidden) {
+        console.log("На странице корзины отображается сообщение об ошибке.");
+      }
+
+      return true;
+    } catch (error) {
+      console.log("Выпадающее меню корзины не отображается.");
+      return false; // Выпадающее меню корзины не открылось или селектор dropdownBasket не отображается
+    }
+  }
+}
--- tests/basket.spec.ts
+++ tests/basket.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect, BrowserContext } from "@playwright/test";
 import BasketPO from "../page-objects/basket";
+import DropDownBasketPO from "../page-objects/dropdownbasket";
 import MainPagePO from "../page-objects/main-page";
 import { LabelEnum } from "../constants/label.enum";
-// import { testConfig } from "../config";
+import { testConfig } from "../config";
 
-let basket: BasketPO, mainPage: MainPagePO, context: BrowserContext;
+let basket: BasketPO, 
+mainPage: MainPagePO, 
+dropdownBasket: DropDownBasketPO, 
+context: BrowserContext;
 
 test.describe("Корзина", () => {
   test.beforeEach(async ({ browser }) => {
@@ -13,122 +17,121 @@ test.describe("Корзина", () => {
     });
     const page = await context.newPage();
     basket = new BasketPO(page);
+    dropdownBasket = new DropDownBasketPO(page);
     mainPage = new MainPagePO(page);
 
     await page.goto("/");
     await page.waitForLoadState("networkidle");
+
+    // expect(await mainPage.isLoggedIn()).toBeFalsy();
     // await mainPage.logIn(testConfig.username, testConfig.password);
 
-    await basket.clearBasket();
-    expect(await basket.isBasketCleared()).toBeTruthy();
+    await dropdownBasket.clearBasket();
+    expect(await dropdownBasket.isBasketCleared()).toBeTruthy();
     expect(await mainPage.isLoggedIn()).toBeTruthy();
-  });
-
-  test.afterEach(async () => {
-    await context.close();
   });
 
   test("Тест-кейс 1. Переход в пустую корзину.", async () => {
     await mainPage.clickBasketBtn();
-    expect(await basket.isDropDownBasketOpened()).toBeTruthy();
+    expect(await dropdownBasket.isDropDownBasketOpened()).toBeTruthy();
 
-    await basket.goToBasketPage();
-    expect(await basket.isBasketPageOpened()).toBeTruthy();
+    await dropdownBasket.goToBasketPage();
+    expect(await dropdownBasket.isBasketPageOpened()).toBeTruthy();
   });
 
   test("Тест-кейс 2. Переход в корзину с 1 неакционным товаром.", async () => {
-    await basket.addNoPromoProductByName(LabelEnum.noPromoProduct_1);
-    const productPrice = await basket.getPriceForProduct(LabelEnum.noPromoProduct_1);
-    expect(await basket.countProductsInBasket()).toEqual("1");
+    await dropdownBasket.addNoPromoProductByName(LabelEnum.noPromoProduct_1);
+    const productPrice = await dropdownBasket.getPriceForProduct(LabelEnum.noPromoProduct_1);
+    expect(await dropdownBasket.countProductsInBasket()).toEqual("1");
 
     await mainPage.clickBasketBtn();
-    expect(await basket.isDropDownBasketOpened()).toBeTruthy();
-    expect(await basket.isProductInBasket(LabelEnum.noPromoProduct_1, productPrice)).toBeTruthy();
+    expect(await dropdownBasket.isDropDownBasketOpened()).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.noPromoProduct_1, productPrice)).toBeTruthy();
 
-    await basket.goToBasketPage();
-    expect(await basket.isBasketPageOpened()).toBeTruthy();
+    await dropdownBasket.goToBasketPage();
+    expect(await dropdownBasket.isBasketPageOpened()).toBeTruthy();
   });
 
   test("Тест-кейс 3. Переход в корзину с 1 акционным товаром.", async () => {
-    await basket.addPromoProductByName(LabelEnum.promoProduct_1);
-    const productPrice = await basket.getPriceForPromoProduct(LabelEnum.promoProduct_1);
-    expect(await basket.countProductsInBasket()).toEqual("1");
+    await dropdownBasket.addPromoProductByName(LabelEnum.promoProduct_1);
+    const productPrice = await dropdownBasket.getPriceForPromoProduct(LabelEnum.promoProduct_1);
+    expect(await dropdownBasket.countProductsInBasket()).toEqual("1");
 
     await mainPage.clickBasketBtn();
-    expect(await basket.isDropDownBasketOpened()).toBeTruthy();
-    expect(await basket.isProductInBasket(LabelEnum.promoProduct_1, productPrice)).toBeTruthy();
-    const totalBasketPrice = await basket.getTotalBasketPrice();
-    const actualTotalPrice = await basket.getActualTotalPrice();
+    expect(await dropdownBasket.isDropDownBasketOpened()).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.promoProduct_1, productPrice)).toBeTruthy();
+    const totalBasketPrice = await dropdownBasket.getTotalBasketPrice();
+    const actualTotalPrice = await dropdownBasket.getActualTotalPrice();
     expect(totalBasketPrice).toEqual(actualTotalPrice);
 
-    await basket.goToBasketPage();
-    expect(await basket.isBasketPageOpened()).toBeTruthy();
+    await dropdownBasket.goToBasketPage();
+    expect(await dropdownBasket.isBasketPageOpened()).toBeTruthy();
   });
 
   test("Тест-кейс 4. Переход в корзину с 9 разными товарами.", async () => {
-    await basket.addPromoProductByName(LabelEnum.promoProduct_1);
-    expect(await basket.countProductsInBasket()).toEqual("1");
+    await dropdownBasket.addPromoProductByName(LabelEnum.promoProduct_1);
+    expect(await dropdownBasket.countProductsInBasket()).toEqual("1");
 
-    await basket.addNoPromoProductByName(LabelEnum.noPromoProduct_1);
-    await basket.addNoPromoProductByName(LabelEnum.noPromoProduct_2);
-    await basket.addNoPromoProductByName(LabelEnum.noPromoProduct_3);
-    await basket.addNoPromoProductByName(LabelEnum.noPromoProduct_4);
-    await basket.addNoPromoProductByName(LabelEnum.noPromoProduct_5);
-    await basket.addPromoProductByName(LabelEnum.promoProduct_2);
-    await basket.addPromoProductByName(LabelEnum.promoProduct_3);
-    const noPromoProduct_1_price = await basket.getPriceForProduct(LabelEnum.noPromoProduct_1);
-    const noPromoProduct_2_price = await basket.getPriceForProduct(LabelEnum.noPromoProduct_2);
-    const noPromoProduct_3_price = await basket.getPriceForProduct(LabelEnum.noPromoProduct_3);
-    const noPromoProduct_4_price = await basket.getPriceForProduct(LabelEnum.noPromoProduct_4);
-    const noPromoProduct_5_price = await basket.getPriceForProduct(LabelEnum.noPromoProduct_5);
-    const promoProduct_1_price = await basket.getPriceForPromoProduct(LabelEnum.promoProduct_1);
-    const promoProduct_2_price = await basket.getPriceForPromoProduct(LabelEnum.promoProduct_2);
-    const promoProduct_3_price = await basket.getPriceForPromoProduct(LabelEnum.promoProduct_3);
+    await dropdownBasket.addNoPromoProductByName(LabelEnum.noPromoProduct_1);
+    await dropdownBasket.addNoPromoProductByName(LabelEnum.noPromoProduct_2);
+    await dropdownBasket.addNoPromoProductByName(LabelEnum.noPromoProduct_3);
+    await dropdownBasket.addNoPromoProductByName(LabelEnum.noPromoProduct_4);
+    await dropdownBasket.addNoPromoProductByName(LabelEnum.noPromoProduct_5);
+    await dropdownBasket.addPromoProductByName(LabelEnum.promoProduct_2);
+    await dropdownBasket.addPromoProductByName(LabelEnum.promoProduct_3);
+    const noPromoProduct_1_price = await dropdownBasket.getPriceForProduct(LabelEnum.noPromoProduct_1);
+    const noPromoProduct_2_price = await dropdownBasket.getPriceForProduct(LabelEnum.noPromoProduct_2);
+    const noPromoProduct_3_price = await dropdownBasket.getPriceForProduct(LabelEnum.noPromoProduct_3);
+    const noPromoProduct_4_price = await dropdownBasket.getPriceForProduct(LabelEnum.noPromoProduct_4);
+    const noPromoProduct_5_price = await dropdownBasket.getPriceForProduct(LabelEnum.noPromoProduct_5);
+    const promoProduct_1_price = await dropdownBasket.getPriceForPromoProduct(LabelEnum.promoProduct_1);
+    const promoProduct_2_price = await dropdownBasket.getPriceForPromoProduct(LabelEnum.promoProduct_2);
+    const promoProduct_3_price = await dropdownBasket.getPriceForPromoProduct(LabelEnum.promoProduct_3);
 
     await mainPage.goToPage(2);
-    await basket.addNoPromoProductByName(LabelEnum.noPromoProduct_6);
-    const noPromoProduct_6_price = await basket.getPriceForProduct(LabelEnum.noPromoProduct_6);
+    await dropdownBasket.addNoPromoProductByName(LabelEnum.noPromoProduct_6);
+    const noPromoProduct_6_price = await dropdownBasket.getPriceForProduct(LabelEnum.noPromoProduct_6);
 
-    expect(await basket.countProductsInBasket()).toEqual("9");
+    expect(await dropdownBasket.countProductsInBasket()).toEqual("9");
 
     await mainPage.clickBasketBtn();
-    expect(await basket.isDropDownBasketOpened()).toBeTruthy();
+    expect(await dropdownBasket.isDropDownBasketOpened()).toBeTruthy();
 
-    expect(await basket.isProductInBasket(LabelEnum.noPromoProduct_1, noPromoProduct_1_price)).toBeTruthy();
-    expect(await basket.isProductInBasket(LabelEnum.noPromoProduct_2, noPromoProduct_2_price)).toBeTruthy();
-    expect(await basket.isProductInBasket(LabelEnum.noPromoProduct_3, noPromoProduct_3_price)).toBeTruthy();
-    expect(await basket.isProductInBasket(LabelEnum.noPromoProduct_4, noPromoProduct_4_price)).toBeTruthy();
-    expect(await basket.isProductInBasket(LabelEnum.noPromoProduct_5, noPromoProduct_5_price)).toBeTruthy();
-    expect(await basket.isProductInBasket(LabelEnum.noPromoProduct_6, noPromoProduct_6_price)).toBeTruthy();
-    expect(await basket.isProductInBasket(LabelEnum.promoProduct_1, promoProduct_1_price)).toBeTruthy();
-    expect(await basket.isProductInBasket(LabelEnum.promoProduct_2, promoProduct_2_price)).toBeTruthy();
-    expect(await basket.isProductInBasket(LabelEnum.promoProduct_3, promoProduct_3_price)).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.noPromoProduct_1, noPromoProduct_1_price)).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.noPromoProduct_2, noPromoProduct_2_price)).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.noPromoProduct_3, noPromoProduct_3_price)).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.noPromoProduct_4, noPromoProduct_4_price)).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.noPromoProduct_5, noPromoProduct_5_price)).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.noPromoProduct_6, noPromoProduct_6_price)).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.promoProduct_1, promoProduct_1_price)).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.promoProduct_2, promoProduct_2_price)).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.promoProduct_3, promoProduct_3_price)).toBeTruthy();
 
-    const totalBasketPrice = await basket.getTotalBasketPrice();
-    const actualTotalPrice = await basket.getActualTotalPrice();
+    const totalBasketPrice = await dropdownBasket.getTotalBasketPrice();
+    const actualTotalPrice = await dropdownBasket.getActualTotalPrice();
     expect(totalBasketPrice).toEqual(actualTotalPrice);
 
-    await basket.goToBasketPage();
-    expect(await basket.isBasketPageOpened()).toBeTruthy();
+    await dropdownBasket.goToBasketPage();
+    expect(await dropdownBasket.isBasketPageOpened()).toBeTruthy();
   });
 
   test("Тест-кейс 5. Переход в корзину с 9 акционными товарами одного наименования.", async () => {
-    await basket.addPromoProductByName(LabelEnum.promoProduct_3, 9);
+    await dropdownBasket.addPromoProductByName(LabelEnum.promoProduct_3, 9);
 
-    const promoProduct_3_price = await basket.getPriceForPromoProduct(LabelEnum.promoProduct_3);
+    const promoProduct_3_price = await dropdownBasket.getPriceForPromoProduct(LabelEnum.promoProduct_3);
 
-    expect(await basket.countProductsInBasket()).toEqual("9");
+    expect(await dropdownBasket.countProductsInBasket()).toEqual("9");
 
     await mainPage.clickBasketBtn();
-    expect(await basket.isDropDownBasketOpened()).toBeTruthy();
+    expect(await dropdownBasket.isDropDownBasketOpened()).toBeTruthy();
 
-    expect(await basket.isProductInBasket(LabelEnum.promoProduct_3, promoProduct_3_price, 9)).toBeTruthy();
+    expect(await dropdownBasket.isProductInBasket(LabelEnum.promoProduct_3, promoProduct_3_price, 9)).toBeTruthy();
 
-    const totalBasketPrice = await basket.getTotalBasketPrice();
-    const actualTotalPrice = await basket.getActualTotalPrice();
+    const totalBasketPrice = await dropdownBasket.getTotalBasketPrice();
+    const actualTotalPrice = await dropdownBasket.getActualTotalPrice();
     expect(totalBasketPrice).toEqual(actualTotalPrice);
 
-    await basket.goToBasketPage();
-    expect(await basket.isBasketPageOpened()).toBeTruthy();
+    await dropdownBasket.goToBasketPage();
+    expect(await dropdownBasket.isBasketPageOpened()).toBeTruthy();
   });
 });
